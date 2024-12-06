0e3e8da3ec461caaf338d85d8b120ae260c864f1
tony <tonyofbyteball@users.noreply.github.com>
2024-12-07 00:47:01 +0800

    explorer front-end on port 3000

--- src/nodes/ObyteExplorer/ObyteExplorer.js
+++ src/nodes/ObyteExplorer/ObyteExplorer.js
@@ -31,7 +31,7 @@ class ObyteExplorer extends AbstractNode {
 	}
 
 	childReady (m) {
-		console.log(`Obyte Explorer is running on http://localhost:${this.webPort}`)
+		console.log(`Obyte Explorer is running on http://localhost:3000`)
 	}
 }
 
